8db6c23646b0ad78a19b3aa38b3ef2f5738533fe
apismensky <alexey.pismenskiy@octanner.com>
2014-10-29 13:22:49 -0600

    Add call to the DB

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,25 @@
 package controllers;
 
+import play.db.*;
 import play.mvc.*;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok("Your new application is ready.");
-    }
+        Connection connection = DB.getConnection();
+        try {
+            ResultSet rs = connection.prepareStatement("SELECT 1 as rs FROM DUAL").executeQuery();
+            rs.next();
+            String result = rs.getString("rs");
+            return ok("Your DB returns " + result);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return internalServerError("DB connection failed: " + e.getMessage());
 
+        }
+    }
 }
